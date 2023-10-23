80c82665d635fe1636ab4ffeb512b372a3c4c8eb
shivam <shivam.srivastava@ksolves.com>
2023-10-23 13:28:35 +0530

    created module calc

--- importExport/index.js
+++
@@ -1,4 +0,0 @@
-const add = require("./oper");
-
-console.log(add(5,5));
-console.log(name);
--- importExport/oper.js
+++
@@ -1,9 +0,0 @@
-const add = () => {
-    return a + b;
-};
-
-const name = "shivam";
-
-// exports
-
-module.exports = name;
