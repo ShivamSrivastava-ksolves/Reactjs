b934fa68ce0fef8cda7a6b918ca9d75020c5ae3b
shivam <shivam.srivastava@ksolves.com>
2023-10-25 10:22:14 +0530

    leftover things

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("First day of training");
- 
+
 // adding two numbers
 // let c = addnumber(1,2,3);
 // console.log(c);
@@ -13,52 +13,45 @@ console.log("First day of training");
 //get element by id
 const testElement = document.getElementById("test");
 const changeTextButton = document.getElementById("changeTextButton");
-changeTextButton.addEventListener("click", function(){
-    testElement.textContent = "ById";
-  });
-
+changeTextButton.addEventListener("click", function () {
+  testElement.textContent = "ById";
+});
 
 //get element by class
 
 const testElements1 = document.getElementsByClassName("classname1");
- const changeTextButton2 = document.getElementById("changeTextButton1");
- changeTextButton2.addEventListener("click", function() {
-
- if (testElements1.length > 0) {
-
- testElements1[0].textContent = "ByClassName";
- } else {
- console.log("No elements with class 'classname1' found.");
- }
+const changeTextButton2 = document.getElementById("changeTextButton1");
+changeTextButton2.addEventListener("click", function () {
+  if (testElements1.length > 0) {
+    testElements1[0].textContent = "ByClassName";
+  } else {
+    console.log("No elements with class 'classname1' found.");
+  }
 });
-
 
 // get element by tag name
 
 const testElement3 = document.getElementsByTagName("p");
 const changeTextButton3 = document.getElementById("changeTextButton2");
-changeTextButton3.addEventListener("click", function(){
-    testElement3[2].style.color = "red";
-  });
+changeTextButton3.addEventListener("click", function () {
+  testElement3[2].style.color = "red";
+});
 
+// get element by query selector
 
-  // get element by query selector
+const testElement4 = document.querySelector(".classname1");
+const changeTextButton4 = document.getElementById("changeTextButton3");
+changeTextButton4.addEventListener("click", function () {
+  console.log(testElement4);
+});
 
+// query selector all
 
-  const testElement4 = document.querySelector(".classname1");
-  const changeTextButton4 = document.getElementById("changeTextButton3");
-  changeTextButton4.addEventListener("click", function(){
-      console.log(testElement4)
-    });
-
-    // query selector all
-
-    const testElement5 = document.querySelectorAll(".classname1");
-  const changeTextButton5 = document.getElementById("changeTextButton4");
-  changeTextButton5.addEventListener("click", function(){
-      console.log(testElement5)
-    });
-
+const testElement5 = document.querySelectorAll(".classname1");
+const changeTextButton5 = document.getElementById("changeTextButton4");
+changeTextButton5.addEventListener("click", function () {
+  console.log(testElement5);
+});
 
 // var let const variables
 
@@ -69,11 +62,10 @@ const z = 30;
 // const z = 40;
 // console.log(z);    it will show error
 
-
 // loops
 
 // for(i=x+1;i<=20;i++){
-//     console.log(i);  
+//     console.log(i);
 // }
 
 // while(x<20){
@@ -81,93 +73,136 @@ const z = 30;
 //     x++;
 // }
 
-
-
 //functions
 
-function callfunct(){
-
-    alert("You called a function");  
-    console.log("function called")
+function callfunct() {
+  alert("You called a function");
+  console.log("function called");
 }
 
 //  array
 
-var array1=["shivam","raj","sumit"];
+var array1 = ["shivam", "raj", "sumit"];
 
-function arrays(){
-    for(i=0;i<=array1.length;i++){
-        console.log(array1[i]);
-    }
+function arrays() {
+  for (i = 0; i <= array1.length; i++) {
+    console.log(array1[i]);
+  }
 }
 
 // objects
 
-emp={id:112,name:"shivam",salary:"20000"};
+emp = { id: 112, name: "shivam", salary: "20000" };
 
-function objects(){
-    
-        console.log(emp.id+" "+emp.name+" "+emp.salary);
-
+function objects() {
+  console.log(emp.id + " " + emp.name + " " + emp.salary);
 }
 
 // rest api is fetched
 
 function fetchDataAndDisplay(url) {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Clicked on service");
-        const dataContainer = document.getElementById("data-container");
-        dataContainer.innerHTML = ""; 
-  
-        if (data.length > 0) {
-          const ul = document.createElement("ul");
-          data.forEach((item) => {
-            const li = document.createElement("li");
-            li.textContent = item.name;
-            ul.appendChild(li);
-          });
-          dataContainer.appendChild(ul);
-        } else {
-          dataContainer.textContent = "No data available.";
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
-  
-  document.getElementById("webbutton").addEventListener("click", () => {
-    fetchDataAndDisplay("https://demo0260207.mockable.io/service1");
-  });
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Clicked on service");
+      const dataContainer = document.getElementById("data-container");
+      dataContainer.innerHTML = "";
 
+      if (data.length > 0) {
+        const ul = document.createElement("ul");
+        data.forEach((item) => {
+          const li = document.createElement("li");
+          li.textContent = item.name;
+          ul.appendChild(li);
+        });
+        dataContainer.appendChild(ul);
+      } else {
+        dataContainer.textContent = "No data available.";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+document.getElementById("webbutton").addEventListener("click", () => {
+  fetchDataAndDisplay("https://demo0260207.mockable.io/service1");
+});
 
 // callbacks in javascript
 var a = 10;
 var b = 20;
-function callfunc(){
-  console.log(a+b);
+function callfunc() {
+  console.log(a + b);
 }
 
-function callfunc2(c,d,callback){
-    console.log(c-d);
-    callback();
+function callfunc2(c, d, callback) {
+  console.log(c - d);
+  callback();
 }
 
-callfunc2(a,b,callfunc);
+callfunc2(a, b, callfunc);
 console.log("Hurray callbacks are working");
+
+// callback hell
+
+var a = 10;
+var b = 20;
+
+function callfunc() {
+  console.log(a + b);
+}
+
+function callfunc2(c, d, callback) {
+  console.log(c - d);
+  callback(function () {
+    console.log("Inside the first nested callback");
+    callback(function () {
+      console.log("Inside the second nested callback");
+      callback(function () {
+        console.log("Inside the third nested callback");
+        console.log("Hurray, deep callback hell!");
+      });
+    });
+  });
+}  
+
+console.log("Before invoking callfunc2");
+callfunc2(a, b, callfunc);
+console.log("Hurray callbacks are working");
+
+
+
+// promises 
+
+let p = new Promise((resolve, reject) => {
+
+  let a =1+1;
+  if(a == 2){
+    resolve('success')
+  }else{
+    reject('Failed')
+  }
   
+  })
+  
+  p.then((message) => {
+    console.log('This is in then' + message)
+  }).catch((message) => {
+    console.log('This in the catch' + message)
+  })
 
 
 
+  // async data fetching in api
 
+  async function getdata(){
+    fetch('https://demo0260207.mockable.io/service1')
+    .then((response)=>{
+        return response.text();
+    }).then((data)=>{
+        console.log(data);
+        fetchdatatext.textContent = "Data fetched, see console";
 
-
-
-
-
-
-
-
-
+    })
+  }
